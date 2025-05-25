20c1f1a02204296b7e7e4f17c4e338637d5343fd
marmooo <marmooo@users.noreply.github.com>
2025-05-26 00:17:19 +0900

    fix TTS on iOS

--- src/index.js
+++ src/index.js
@@ -59,6 +59,10 @@ function createAudioContext() {
 }
 
 function unlockAudio() {
+  const uttr = new SpeechSynthesisUtterance("");
+  uttr.lang = "en-US";
+  speechSynthesis.speak(uttr);
+
   if (audioContext) {
     audioContext.resume();
   } else {
@@ -67,7 +71,7 @@ function unlockAudio() {
     loadAudio("error", "mp3/cat.mp3");
     loadAudio("correct", "mp3/correct3.mp3");
   }
-  document.removeEventListener("pointerdown", unlockAudio);
+  document.removeEventListener("click", unlockAudio);
   document.removeEventListener("keydown", unlockAudio);
 }
 
@@ -537,5 +541,5 @@ document.getElementById("kohacu").onclick = catNyan;
 document.getElementById("respeak").onclick = respeak;
 document.getElementById("startVoiceInput").onclick = startVoiceInput;
 document.getElementById("stopVoiceInput").onclick = stopVoiceInput;
-document.addEventListener("pointerdown", unlockAudio, { once: true });
+document.addEventListener("click", unlockAudio, { once: true });
 document.addEventListener("keydown", unlockAudio, { once: true });
